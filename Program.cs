79ef033459a12c38e4154caff2ec2dc4b9c1ca9d
kurisu <kamleshkmistry33@gmail.com>
2022-11-17 20:27:45 +0530

    new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	new file:   Program.cs 	new file:   XO.csproj 	new file:   bin/Debug/net7.0/XO.deps.json 	new file:   bin/Debug/net7.0/XO.dll 	new file:   bin/Debug/net7.0/XO.exe 	new file:   bin/Debug/net7.0/XO.pdb 	new file:   bin/Debug/net7.0/XO.runtimeconfig.json 	new file:   obj/Debug/net7.0/.NETCoreApp,Version=v7.0.AssemblyAttributes.cs 	new file:   obj/Debug/net7.0/XO.AssemblyInfo.cs 	new file:   obj/Debug/net7.0/XO.AssemblyInfoInputs.cache 	new file:   obj/Debug/net7.0/XO.GeneratedMSBuildEditorConfig.editorconfig 	new file:   obj/Debug/net7.0/XO.GlobalUsings.g.cs 	new file:   obj/Debug/net7.0/XO.assets.cache 	new file:   obj/Debug/net7.0/XO.csproj.AssemblyReference.cache 	new file:   obj/Debug/net7.0/XO.csproj.CoreCompileInputs.cache 	new file:   obj/Debug/net7.0/XO.csproj.FileListAbsolute.txt 	new file:   obj/Debug/net7.0/XO.dll 	new file:   obj/Debug/net7.0/XO.genruntimeconfig.cache 	new file:   obj/Debug/net7.0/XO.pdb 	new file:   obj/Debug/net7.0/apphost.exe 	new file:   obj/Debug/net7.0/ref/XO.dll 	new file:   obj/Debug/net7.0/refint/XO.dll 	new file:   obj/XO.csproj.nuget.dgspec.json 	new file:   obj/XO.csproj.nuget.g.props 	new file:   obj/XO.csproj.nuget.g.targets 	new file:   obj/project.assets.json 	new file:   obj/project.nuget.cache

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+/*Console.WriteLine("Hello, World!");
+Console.WriteLine("This is C# lol!");
+
+string num;
+num = "this is C#!";
+
+Console.WriteLine("Oh lol " + num);
+
+int i = 34123;
+float F =  i;       //12e5f;
+Console.WriteLine("F: " + F);*/
+
+/*int j = 3354; //bool B = true;
+Console.WriteLine(Convert.ToString(j));*/
+
+Console.Write("Name: ");
+string S  = Console.ReadLine();
+
+Console.Write("Age: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Name: " + S);
+Console.WriteLine("Age: " + A );
+
+for(int i = A; i <= 20 ; i++){
+    Console.WriteLine(i);
+}
+
+
+/*using System;
+
+namespace Program
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");    
+    }
+  }
+}*/
