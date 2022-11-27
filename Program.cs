9a8775e89072767d1c8a4ff9c9997cc4839c19ed
kurisu <kamleshkmistry33@gmail.com>
2022-11-27 20:48:16 +0530

    modified:   Program.cs 	modified:   bin/Debug/net7.0/XO.dll 	modified:   bin/Debug/net7.0/XO.pdb 	modified:   obj/Debug/net7.0/XO.dll 	modified:   obj/Debug/net7.0/XO.pdb 	modified:   obj/Debug/net7.0/ref/XO.dll 	modified:   obj/Debug/net7.0/refint/XO.dll 	modified:   swap.cs

--- Program.cs
+++ Program.cs
@@ -1,52 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c;
 
+c = a;
+a = b;
+b = c;
 
-/*Console.WriteLine("Hello, World!");
-Console.WriteLine("This is C# lol!");
-
-string num;
-num = "this is C#!";
-
-Console.WriteLine("Oh lol " + num);
-
-int i = 34123;
-float F =  i;       //12e5f;
-Console.WriteLine("F: " + F);*/
-
-/*int j = 3354; //bool B = true;
-Console.WriteLine(Convert.ToString(j));*/
-
-Console.Write("Name: ");
-string S  = Console.ReadLine();
-
-/*Console.Write("Age: ");
-int A = Convert.ToInt32(Console.ReadLine());*/
-int A = 10;
-//int *p;
-//p = &A;
-
-//Console.Write(*p);
-
-Console.WriteLine("Name: " + S);
-Console.WriteLine("Age: " + A );
-
-/*Console.Write("Enter N:");
-int n = Convert.ToInt32(Console.ReadLine());
-
-for(int i = A; i <= n ; i++){
-    Console.WriteLine(A);
-}*/
-
-
-/*using System;
-
-namespace Program
-{
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      Console.WriteLine("Hello World!");    
-    }
-  }
-}*/
+Console.WriteLine("A = " + a);
+Console.WriteLine("B = " + b);
